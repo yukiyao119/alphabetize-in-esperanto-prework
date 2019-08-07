54a774abcd2ebfc12fa4252b58268ebacbb542ed
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-07 04:50:11 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -5,7 +5,7 @@ require "pry"
 def alphabetize(arr)
   esperanto_arr = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars  # esperanto_arr.index {|x| x == "b"}  #=> 1   length 28, index 0-27
   result = []
-  inner_arr = arr.
+  inner_arr = arr.slice(1, arr.length-1)
   binding.pry
   arr.each_with_index do |word, index| 
     # word[index]  = "m"
