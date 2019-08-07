30ffdd6cbacfecb23d3fb4d87a104b282cc030da
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-07 05:18:50 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -3,31 +3,8 @@ require "pry"
 # ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
 
 def alphabetize(arr)
-  esperanto_arr = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars  # esperanto_arr.index {|x| x == "b"}  #=> 1   length 28, index 0-27
-  result = []
-  new_arr = arr[1..]
-  arr.each_with_index do |word, index| 
-    new_arr.each_with_index do |value, i|      # value[i] = "b" 
-      if word[index] == value[i]
-        index += 1 
-        i += 1
-      elsif word[index] != value[i]
-        i_in_esperanto1 = esperanto_arr.index(word[index])
-        i_in_esperanto2 = esperanto_arr.index(value[i])
-        # binding.pry
-        if i_in_esperanto1 - i_in_esperanto2 < 0 
-          result << word 
-          result << value 
-        else 
-          result << value
-          result << word
-        end 
-      end 
-      break
-      # binding.pry
-    end 
-  end 
-  result
+  esperanto_arr = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+
 end
 # g_words = ["ĝuste", "ĝis revido"]
 # sorted_g_words = ["ĝis revido", "ĝuste"]
