07a3cc6c6699e4f4ae836c5aca955033ecab947a
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-07 04:36:01 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,13 +4,19 @@ require "pry"
 
 def alphabetize(arr)
   esperanto_arr = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars  # esperanto_arr.index {|x| x == "b"}  #=> 1   length 28, index 0-27
-  
+  result = []
   arr.each_with_index do |word, index| 
     # word[index]  = "m"
     # i_in_esperanto = esperanto_arr.index()
-    arr.shift!
-    
-    binding.pry
+    arr.shift!.each_with_index do |value, i|
+      # value[i] = "b" 
+      if word[index] != value[i]
+        if esperanto_arr.index(word[index]) < esperanto_arr.index(value[i])
+          result << word
+        end 
+      end 
+      binding.pry
+    end 
   end 
 end
 # g_words = ["ĝuste", "ĝis revido"]
