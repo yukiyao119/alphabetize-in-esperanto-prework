2fac7e0886c363d2b12c126bb722275a11a2f7ae
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-08-07 04:14:23 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -6,11 +6,12 @@ def alphabetize(arr)
   esperanto_arr = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars  # esperanto_arr.index {|x| x == "b"}  #=> 1   length 28, index 0-27
   result = []
   i_in_esperanto = 0
-  arr.each do |word| 
-    i_in_esperanto = esperanto_arr.index(word.split("").first)
-    # "m" is 16
-    binding.pry
-  end 
+  arr.split("")
+  # arr.each do |word| 
+  #   i_in_esperanto = esperanto_arr.index(word.split("").first)
+  #   # "m" is 16
+  #   binding.pry
+  # end 
 end
 # g_words = ["ĝuste", "ĝis revido"]
 # sorted_g_words = ["ĝis revido", "ĝuste"]
